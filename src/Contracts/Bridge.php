c2c06b42107488f6419481ff155f0141fbed4433
maksmartyn <maksmartyn@protonmail.com>
2023-06-14 17:15:17 +0700

    feat: Добавлен функционал логирования и отладки
    Новое:
    - метод setLogChannel(string 'my-channel-name') устанавливает канал логирования для фасада Log. По умолчанию 'stack';
    - метод setDebug(bool $debug) включает режим отладки - логирование всех запросов, трасировок и ответов сервера;
    - метод sendRequest(string $method, string $path, array $data = []) реализует отправку запроса с использованием логирования и отладки

--- src/Contracts/Bridge.php
+++ src/Contracts/Bridge.php
@@ -2,6 +2,26 @@
 
 namespace Atwinta\Bridges\Contracts;
 
+use Illuminate\Http\Client\Response;
+
 interface Bridge
 {
+    /**
+     * TODO: для пхп 8+ добавить сигнатуру методу
+     *
+     * @return static
+     */
+    public function setDebug(bool $debug);
+
+    /**
+     * TODO: для пхп 8+ добавить сигнатуру методу
+     *
+     * @return static
+     */
+    public function setLogChannel(string $logChannel);
+
+    /**
+     * @phpstan-param array<int|string,mixed> $data
+     */
+    public function sendRequest(string $method, string $path, array $data = []): ?Response;
 }
